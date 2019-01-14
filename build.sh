a9f018d784ccdb680857762bd621a8670bc8ff26
Willy Romão <willyr.goncalves@gmail.com>
2019-01-14 08:36:07 -0200

    build for linux and freebsd

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-GOBUILDVERSION="$(go run go-radius-gen-acct.go -v 2> /dev/null | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"; GOOS=linux; GOARCH=amd64; GOOS="$GOOS" GOARCH="$GOARCH" go build -o go-radius-gen-acct-"$GOBUILDVERSION"-"$GOOS"-"$GOARCH"
+pGOOS=(linux freebsd)
+GOARCH=amd64 
+GOBUILDVERSION="$(go run go-radius-gen-acct.go -v 2> /dev/null | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"
+
+for GOOS in "${pGOOS[@]}"; do
+  GOOS="$GOOS" GOARCH="$GOARCH" go build -o go-radius-gen-acct-"$GOBUILDVERSION"-"$GOOS"-"$GOARCH"
+done
